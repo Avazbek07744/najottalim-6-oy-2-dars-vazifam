b2a3a77f6c156cd0520d50f8121a1d78a2dce056
Avazbek07744 <javazbek87@gmail.com>
2024-09-17 15:46:31 +0500

    vazifam

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import data from './assets/data.json'
 
 const App = () => {
   return (
-    <div className=' p-5 w-[1400px] m-auto mt-10 flex gap-10 flex-wrap rounded-xl'>
-      {data.map((obj)=> <User data={obj}/>)}
+    <div className='bg-blue-500'>
+      <div className=' w-[1350px] m-auto pt-10 flex gap-10 flex-wrap rounded-xl '>
+        {data.map((obj) => <User data={obj} />)}
+      </div>
     </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,8 @@ const Card = ({ data }) => {
     const { firstName, lastName, image, phone, email, address, cars } = data
 
     return (
-        <div className='w-max m-auto border border-black rounded-xl bg-[#00FFFF] p-5 hover transition duration-700 text-gray-500'>
-            <img className='w-64' src={image} alt="" />
+        <div className='w-max m-auto w-[420px] rounded-xl bg-[#00FFFF] p-5 hover transition duration-700 text-gray-500'>
+            <img className='w-96 m-auto' src={image} alt="" />
             <span className='text-center flex gap-5 justify-center mt-3 mb-5'>
                 <h1>Isim: {lastName}</h1>
                 <h1>Familiya: {firstName}</h1>
@@ -15,15 +15,27 @@ const Card = ({ data }) => {
             <div className='flex gap-20 mt-3 mb-5'>
                 <p>Adress:</p>
                 <div>
-                    <p >Region: {address.region}</p>
-                    <p>Zip: {address.zip}</p>
+                    <span className='flex gap-1'>
+                        <input type="checkbox" name="" id="salom" />
+                        <p>Region: {address.region}</p>
+                    </span>
+                    <span className='flex gap-1'>
+                        <input type="checkbox" name="" id="alik" />
+                        <p>Zip: {address.zip}</p>
+                    </span>
                 </div>
             </div>
             <div className="row">
                 {cars.length > 0 ? (
                     <div className="flex gap-20">
-                        <p>{cars[0]}</p>
-                        <p>{cars[1]}</p>
+                        <span className='flex gap-2'>
+                            <input type="checkbox" name="" id="salom" />
+                            <p>{cars[0]}</p>
+                        </span>
+                        <span className='flex gap-2'>
+                            <input type="checkbox" name="" id="alik" />
+                            <p>{cars[1]}</p>
+                        </span>
                     </div>
                 ) : <p>😿😥😥😥😿</p>}
             </div>
